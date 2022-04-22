e3607573f4f8ca506662b0067dbec1edef3945cb
iagolirapassos <iagolirapassos@gmail.com>
2022-04-21 22:56:23 -0300

    Adição do comando blocoExecutar

--- examples/SaidaBlink/SaidaBlink.ino
+++ examples/SaidaBlink/SaidaBlink.ino
@@ -4,9 +4,9 @@ byte Leds[] = {3, 4, 5, 6, 7}; //Número dos pinos
 bool pinStateIn[] = {LOW, LOW, LOW, LOW, LOW}; //Estados iniciais
 
 int blocoComandos[2][5] = {
-							{1, 1, 1, 1, 1}, //Bloco de estados 1
-							{0, 0, 0, 0, 0} //Bloco de estados 2
-						  };
+                            {1, 1, 1, 1, 1}, //Bloco de estados 1
+                            {0, 0, 0, 0, 0} //Bloco de estados 2
+                          };
 //Tamanho do vetor Leds igual a 5 ou
 int lenLeds = sizeof(Leds)/sizeof(Leds[0]); //Forma de determinar o tamanho de um vetor
 
@@ -18,7 +18,7 @@ void setup(){
 
 void loop()
 {
-	//800 é o delay em ms
-	Arduino.blocoExecutar(blocoComandos[0], 800); //Executar Bloco de estados 1
-	Arduino.blocoExecutar(blocoComandos[1], 800); //Executar Bloco de estados 2
+  //800 é o delay em ms
+  Arduino.blocoExecutar(blocoComandos[0], 800); //Executar Bloco de estados 1
+  Arduino.blocoExecutar(blocoComandos[1], 800); //Executar Bloco de estados 2
 }
